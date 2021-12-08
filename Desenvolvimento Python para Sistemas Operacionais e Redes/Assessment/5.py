5d6f1b767ab7c92d3499c2caa9191d577dce9247
lucasrbueno <lucasbueno2@hotmail.com>
2021-12-07 23:06:47 -0300

    mais ex

--- Desenvolvimento Python para Sistemas Operacionais e Redes/Assessment/5.py
+++ Desenvolvimento Python para Sistemas Operacionais e Redes/Assessment/5.py
@@ -1,33 +1,64 @@
-def read_file(file):
-  with open(file, "r") as inFile:
-    for column in inFile:
-      yield column
+arquivoA_arquivo = []
+arquivoA_arquivo.append(open("a.txt", "r").readlines())
+arquivoA_arquivo.append(open("b.txt", "r").readlines())
 
-file_list = ["a.txt", "b.txt"]
-file_generators = [read_file(path) for path in file_list]
+arquivoA = []
+arquivoB = []
+
+for item in range(len(arquivoA_arquivo)):
+  if item == 0:
+    for jtem in arquivoA_arquivo[item]:
+      arquivoA = jtem.split(' ')
+  elif item == 1:
+    for jtem in arquivoA_arquivo[item]:
+      arquivoB = jtem.split(' ')
+arquivoA_arquivo.clear()
+
+def lista_maior_menor(arquivoA, arquivoB):
+  if len(arquivoA) > len(arquivoB):
+    item = len(arquivoA) - len(arquivoB)
+    while item != 0:
+        arquivoB.append('0')
+        item -= 1
+  else:
+    item = len(arquivoB) - len(arquivoA)
+    while item != 0:
+        arquivoA.append('0')
+        item -= 1
+
+  return arquivoA, arquivoB
+
+arquivoA, arquivoB = lista_maior_menor(arquivoA, arquivoB)
+
+def somados(arquivoA, arquivoB):
+  for i in range(len(arquivoA)):
+      print(arquivoA[i], " + ", arquivoB[i], " = ", int(arquivoA[i]) + int(arquivoB[i]))
+
+somados(arquivoA, arquivoB)
 
 
-# def countList(file_list):
-#     if len(file_list[0]) > len(file_list[1]):
-#         index = len(file_list[0]) - len(file_list[1])
-#         while index != 0:
-#             listB.append('0')
-#             index -= 1
-#     else:
-#         index = len(listB) - len(listA)
-#         while index != 0:
-#             listA.append('0')
-#             index -= 1
-#     return listA, listB
+  
 
-# def itemsAddUp(file_list[0], file_list[1]):
-#     for i in range(len(listA)):
-#         print(f"{listA[i]} + {listB[i]} = {int(listA[i]) + int(listB[i])}")
+# arquivoB = arquivoA_arquivo[1].ReadLines()
+# arquivoA.split(' '), arquivoB.split(' ')
+
+# file_generators = [read_file(path) for path in arquivoA_arquivo]
 
 
-with open("totals.txt", "w+") as outFile:
-  while True:
-    try:
-      outFile.write(f"{sum([int(next(gen)) for gen in file_generators])}\n")
-    except StopIteration:
-      break
+
+# print(lista_maior_menorA(arquivoA_arquivo))
+
+
+
+
+
+
+
+# with open("totals.txt", "w+") as outFile:
+#   while True:
+#     try:
+#       outFile.write(f"{sum([int(next(gen)) for gen in file_generators])}\n")
+#       # outFile.write(somados(arquivoA_arquivo))
+      
+#     except StopIteration:
+#       break
